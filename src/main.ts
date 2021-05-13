0b434d3c8e627ac3265626ac9d095c6ec13abc7c
Dénes Márk <legekka@gmail.com>
2021-05-13 18:16:18 +0200

    Add port config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  const configService: ConfigService = app.get('ConfigService');
+  
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
